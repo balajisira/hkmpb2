bc2e5910bcbb3785de328981cee93bbe460e0382
=Balaji <=>
2016-11-15 09:04:08 +0530

    post request

--- src/main/java/com/market/wm/AuthService.java
+++ src/main/java/com/market/wm/AuthService.java
@@ -1,5 +1,6 @@
 package com.market.wm;
 
+import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,6 +17,9 @@ public class AuthService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response getUserRole(String user) {
 		String result = "Success login : " + user;
+		System.out.println(">>>>> in getUserRole :" + user);
+//		JsonObject requestObject 
+//		userDao.getUser(emailId);
 		return Response.status(201).entity(result).build();
 	}
 }
